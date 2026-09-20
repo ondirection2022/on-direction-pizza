85ba991f9af59d912d5ce3a752618c370f52a151
ondirection2022 <abdalrahmanshereet3@gmail.com>
2026-09-20 15:53:03 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,12 @@
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyD9uj6RoyXTE8vPk9A-xOZtV_x92j0LQpA",
+  authDomain: "on-direction-pizza-cde2e.firebaseapp.com",
+  databaseURL: "https://on-direction-pizza-cde2e-default-rtdb.firebaseio.com",
+  projectId: "on-direction-pizza-cde2e",
+  storageBucket: "on-direction-pizza-cde2e.firebasestorage.app",
+  messagingSenderId: "469068764271",
+  appId: "1:469068764271:web:a35124ebdea0cfca63a99a",
+  measurementId: "G-GS6ERWK00K"
+};
+
